8328139837d3ffdf291a906959ee56597b5ae75d
mario2598 <43936591+mario2598@users.noreply.github.com>
2018-10-20 09:00:24 -0600

    jasper

--- src/cineuna/controller/LogInController.java
+++ src/cineuna/controller/LogInController.java
@@ -5,6 +5,7 @@
  */
 package cineuna.controller;
 
+import cineuna.model.cobro;
 import cineuna.util.AppContext;
 import cineuna.util.FlowController;
 import cineuna.util.Mensaje;
@@ -13,12 +14,20 @@ import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 import java.util.ResourceBundle;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.event.ActionEvent;
@@ -43,7 +52,17 @@ import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
 import javax.xml.parsers.ParserConfigurationException;
+import net.sf.jasperreports.engine.JREmptyDataSource;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.xml.sax.SAXException;
+import java.util.Map;
+import java.util.HashMap;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperReport;
 
 /**
  * FXML Controller class
@@ -99,10 +118,11 @@ public class LogInController extends Controller implements Initializable {
     @FXML
     private void irMain(ActionEvent event) {
         try{
-           Boolean veri = sendEmail();
+         /*  Boolean veri = sendEmail();
            if (veri) {
                new Mensaje().showModal(Alert.AlertType.ERROR, "Correo prueba", (Stage) root.getScene().getWindow(), "Se envio.");
-           }
+           }*/
+           generarReport();
         if (txtUsuario.getText() == null || txtUsuario.getText().isEmpty()) {
                 new Mensaje().showModal(Alert.AlertType.ERROR, "Validación de usuario", (Stage) root.getScene().getWindow(), "Es necesario digitar un usuario para ingresar al sistema.");
             } else if (txtClave.getText() == null || txtClave.getText().isEmpty()) {
@@ -300,5 +320,26 @@ public class LogInController extends Controller implements Initializable {
         }
 
     }
+     public void generarReport() throws JRException, FileNotFoundException{
+         String userHomeDirect = System.getProperty("user.home");
+         String outPutFile = "src/cineuna/jasperReport/jasperPrueba.pdf";
+         List<cobro> list = new ArrayList<>();
+         
+         cobro c1 = new cobro("25/01/1998",15000);
+         cobro c2 = new cobro("25/01/2018",25000);
+         list.add(c1);
+         list.add(c2);
+        // JRBeanCollectionDataSource cobrojrb = new JRBeanCollectionDataSource(list);
+         JasperReport jasperReport = JasperCompileManager.compileReport("src/cineuna/jasperReport/ReporteGanancias=!!.jrxml");
+         Map<String, Object> parametros = new HashMap<>();
+         Map<String, Object> parametros1 = new HashMap<>();
+         parametros1.put("idPar",22);
+       //  parametros.put("dataSource", cobrojrb);
+         JasperPrint jasperprint = JasperFillManager.fillReport(jasperReport, parametros1, new JREmptyDataSource());
+         
+         OutputStream outputStream = new FileOutputStream(new File(outPutFile));
+            /* Write content to PDF file */
+         JasperExportManager.exportReportToPdfStream(jasperprint, outputStream);
+     }
 
 }
